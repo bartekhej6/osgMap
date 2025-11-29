5731aadf7281eeb7adf3723ddf46833c22d44d9c
Marcin Hajder <marcin.hajder@gmail.com>
2025-11-29 12:27:20 +0100

    Corrections related to meta-data processing

--- src/buildings.cpp
+++ src/buildings.cpp
@@ -8,11 +8,85 @@
 #include <osg/MatrixTransform>
 #include <osg/ShapeDrawable>
 
+#include <osgSim/ShapeAttribute>
+
 #include <iostream>
 
 #include "common.h"
 
 using namespace osg;
+
+// klasa typu NodeVisitor to wzorzec projektowy - warto znaæ!
+// Zadaniem glasy jest odwiedziæ wszystkie wêz³y drzewa. Specjalizacja
+// tej klasy mo¿e dokonaæ odpowiednich zmian np zmieniæ pozycjê wierzcho³ków,
+// dodaæ now¹ geometriê, zmieniæ stany wyœwietlania itd
+class GeomVisitor : public osg::NodeVisitor
+{
+
+public:
+
+    GeomVisitor(/* add your params */) :
+        osg::NodeVisitor( NodeVisitor::TRAVERSE_ALL_CHILDREN )
+    {
+    }
+    //////////////////////////////////////////////////////////////////////////////
+    void apply( osg::Node & node)
+    {
+        traverse(node);
+    }
+    //////////////////////////////////////////////////////////////////////////////
+    void apply( osg::Geode & node)
+    {
+        for ( unsigned i=0; i<node.getNumDrawables(); i++ ) {
+            osg::Geometry* geom = dynamic_cast <osg::Geometry*> (node.getDrawable(i));
+
+            if ( !geom ) continue;
+
+            osg::Vec3Array* verts = (osg::Vec3Array*) geom->getVertexArray();
+
+            for ( unsigned j=0; j<verts->size(); j++ ) {
+                // tutaj mo¿na zmodyfikowaæ wierzcho³ek
+#if 0
+                (*verts)[j] = osg::Vec3 ();
+#endif
+            }
+        }
+    }
+};
+
+
+
+void parse_meta_data(osg::Node* model)
+{
+    osg::Group * group = model->asGroup();
+    GeomVisitor gv;
+
+    for (unsigned i = 0; i < group->getNumChildren(); i++)
+    {
+        osg::Node* kido = group->getChild(i);
+        osgSim::ShapeAttributeList * sal = (osgSim::ShapeAttributeList*)kido->getUserData();
+        if (!sal)
+            continue;
+
+        float height = 0.f;
+        for (unsigned j = 0; j < sal->size(); j++)
+        {
+            // wysokoœci budynkow - przechodzimy wszystkie atrybuty 
+            // w poszukiwaniu atrybutu o nazwie "height"
+            if ((*sal)[j].getName().find("height") != std::string::npos)
+            {
+                if ((*sal)[j].getType() == osgSim::ShapeAttribute::DOUBLE)
+                    height = float((*sal)[j].getDouble()) / 100.f; // cm
+                else if ((*sal)[j].getType() == osgSim::ShapeAttribute::INTEGER)
+                    height = float((*sal)[j].getInt()) / 100.f; // cm
+                break;
+            }
+        }
+
+        kido->accept(gv);
+    }
+}
+
 
 osg::Node* process_buildings(osg::Matrixd& ltw, const std::string & file_path)
 {
@@ -26,12 +100,17 @@ osg::Node* process_buildings(osg::Matrixd& ltw, const std::string & file_path)
         return nullptr;
     }
 
+    // Transformacja ze wspó³rzêdnych geograficznych (GEO) do wspó³rzêdnych œwiata (WGS)
     ConvertFromGeoProjVisitor<true> cfgp;
     buildings_model->accept(cfgp);
 
     WorldToLocalVisitor ltwv(ltw, true);
     buildings_model->accept(ltwv);
 
+#if 0
+    // dokonuj dodatkowego przetwarzania wierzcho³ków po transformacji z uk³adu Geo do WGS
+    parse_meta_data(buildings_model);
+#endif
 
 
 
--- src/landuse.cpp
+++ src/landuse.cpp
@@ -8,11 +8,40 @@
 #include <osg/MatrixTransform>
 #include <osg/ShapeDrawable>
 
+#include <osgSim/ShapeAttribute>
+
 #include <iostream>
+#include <map>
 
 #include "common.h"
 
 using namespace osg;
+
+using Mapping = std::map<std::string, std::vector<osg::ref_ptr<osg::Node>>>;
+
+void parse_meta_data(osg::Node* model, Mapping & umap)
+{
+    osg::Group * group = model->asGroup();
+
+    for (unsigned i = 0; i < group->getNumChildren(); i++)
+    {
+        osg::Node* kido = group->getChild(i);
+        osgSim::ShapeAttributeList * sal = (osgSim::ShapeAttributeList*)kido->getUserData();
+        if (!sal)
+            continue;
+
+        for (unsigned j = 0; j < sal->size(); j++)
+        {
+            // sprawdzamy czy atrybut nazywa siê "fclass"
+            // dla terenu to opis typu kultury
+            if ((*sal)[j].getName().find("fclass") != std::string::npos)
+            {
+                if ((*sal)[j].getType() == osgSim::ShapeAttribute::STRING)
+                    umap[(*sal)[j].getString()].push_back(kido);
+            }
+        }
+    }
+}
 
 osg::Node* process_landuse(osg::Matrixd& ltw, const std::string & file_path)
 {
@@ -36,6 +65,7 @@ osg::Node* process_landuse(osg::Matrixd& ltw, const std::string & file_path)
         osg::DegreesToRadians(mgbb.center().x()), 0.0, ltw);
 
 
+    // Transformacja ze wspó³rzêdnych geograficznych (GEO) do wspó³rzêdnych œwiata (WGS)
     ConvertFromGeoProjVisitor<true> cfgp;
     land_model->accept(cfgp);
 
@@ -44,16 +74,16 @@ osg::Node* process_landuse(osg::Matrixd& ltw, const std::string & file_path)
 
 
 
+#if 0
+    // dokonuj dodatkowego przetwarzania wierzcho³ków po transformacji z uk³adu Geo do WGS
+    Mapping umap;
+    parse_meta_data(land_model, umap);
+#endif
 
 
 
 
     // GOOD LUCK!
-
-
-
-
-
 
 
 
